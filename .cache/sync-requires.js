13111b5ad15f2639028edaa5936101cfed48fef5
gmarin93 <arias.fex@gmail.com>
2021-07-09 15:19:31 -0600

    Third commit

---
+++ .cache/sync-requires.js
@@ -0,0 +1,11 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/home/gmarin/Documentos/Software-GoLabs/project1gatsbyagility/agilitygatsbyproject/.cache/dev-404-page.js")),
+  "component---src-agility-components-dynamic-preview-page-js": preferDefault(require("/home/gmarin/Documentos/Software-GoLabs/project1gatsbyagility/agilitygatsbyproject/src/agility/components/DynamicPreviewPage.js")),
+  "component---src-agility-page-jsx": preferDefault(require("/home/gmarin/Documentos/Software-GoLabs/project1gatsbyagility/agilitygatsbyproject/src/AgilityPage.jsx"))
+}
+
--- src/components/agility-pageModules/Heading.jsx
+++ src/components/agility-pageModules/Heading.jsx
@@ -6,7 +6,7 @@ const Heading = ({ module }) => {
   return (
     <div className="relative px-8">
       <div className="max-w-screen-xl mx-auto my-12 md:mt-18 lg:mt-20">
-        <h1 className="font-display text-secondary-500 text-4xl font-black tracking-wide">
+        <h1 className="font-display text-secondary-500 text-4xl font-blue tracking-wide">
           {customFields.title}
         </h1>
       </div>
